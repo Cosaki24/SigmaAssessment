25302365f7f35586025594a5e46e49ecc6516c6b
Collins Kipepe <ckipepe@softnet.co.tz>
2024-06-07 17:03:45 +0300

    Removed weather forecast functionality, added Swagger and logging services

--- SigmaAssessment/Program.cs
+++ SigmaAssessment/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    options =>
+    {
+        options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+        {
+            Version = "v1",
+            Title = "Candidates API",
+            Description = "A simple API to manage candidates"
+        });
+
+        var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+        options.EnableAnnotations();
+    }
+);
+
+builder.Services.Configure<ApiBehaviorOptions>(Options =>
+{
+    Options.SuppressModelStateInvalidFilter = true;
+});
+
+builder.Services.AddLogging();
 
 var app = builder.Build();
 
